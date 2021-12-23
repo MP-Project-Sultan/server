6a0d3148ade1d280ea31ee103e3b75c38c989df2
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-12-23 11:39:48 +0300

    add like controller & route

---
+++ routers/controllers/like.js
@@ -0,0 +1,36 @@
+const { response } = require("express");
+const likeModel = require("../../db/models/like");
+const postModel = require("../../db/models/post");
+
+const addLike = (req, res) => {
+  const { postId } = req.body;
+  const newlike = new likeModel({
+    postId: postId,
+    userId: req.token.id,
+  });
+  newlike.save().then((result) => {
+    postModel
+      .findByIdAndUpdate(postId, { $push: { like: result._id } })
+      .then((result) => {
+        res.status(201).json(result);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+};
+const deleteLike = (req, res) => {
+  const { id } = req.body;
+  likeModel
+    .findByIdAndDelete({ _id: id })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json("Dislike");
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+module.exports = { addLike, deleteLike };
