e0323ea9c63f260487d85fe45a5160affc9444f5
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2022-01-17 16:05:34 +0300

    Final Update Done !

--- routers/routes/comment.js
+++ routers/routes/comment.js
@@ -16,6 +16,6 @@ commentRouter.get("/getComments/:id", getCommentsForPost);
 
 commentRouter.get("/getComment/:id", getCommentById);
 commentRouter.put('/updateComment/:id',authentication , updateComment)
-commentRouter.delete('/deleteComment/:id',authentication, deleteComment)
+commentRouter.put('/deleteComment/:id',authentication, deleteComment)
 
 module.exports = commentRouter
